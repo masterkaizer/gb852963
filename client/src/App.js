07424026f1357195d2a4248536193a0efde5cb2d
Your Name <brandon.sellam@gmail.com>
2020-06-18 03:03:39 +0200

    j

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Home from '../src/components/home';
 import { createMuiTheme } from '@material-ui/core/styles';
