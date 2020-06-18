e2fcc36c9a2ee4e4f02e2040422b43825ac4c084
Brandon sellam <brandon.sellam@gmail.com>
2020-06-18 02:19:42 +0200

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-  database: process.env.MONGODB_URI || 'mongodb://localhost:27017/books',
+  database: process.env.MONGODB_URI || 'mongodb://bbsb65:Blaugranna-19@ds225038.mlab.com:25038/heroku_x9k82rmj',
 }
