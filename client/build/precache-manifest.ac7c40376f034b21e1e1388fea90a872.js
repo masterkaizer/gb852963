10443285b5723136528e47ccba41f4ccaaf55712
Your Name <brandon.sellam@gmail.com>
2020-06-21 05:57:21 +0200

    u

---
+++ client/build/precache-manifest.ac7c40376f034b21e1e1388fea90a872.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce90971bbf3b369221279edd216a6c3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b46955786a9d614cff3",
+    "url": "/static/css/main.3fa15e66.chunk.css"
+  },
+  {
+    "revision": "3dcbb91826139833f785",
+    "url": "/static/js/2.60dd5432.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.60dd5432.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b46955786a9d614cff3",
+    "url": "/static/js/main.faa9a1b0.chunk.js"
+  },
+  {
+    "revision": "228efc25977fc7b53c58",
+    "url": "/static/js/runtime-main.105206bc.js"
+  }
+]);
