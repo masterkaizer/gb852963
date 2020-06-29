b2a651a6a2d8434f307bc165c20bb84883ae99be
Brandon sellam <brandon.sellam@gmail.com>
2020-06-29 22:53:57 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,38 +1,22 @@
-const express = require('express');
-const cors = require('cors');
-const bodyparser = require('body-parser');
-const nocache = require('nocache');
+const express = require("express");
 
+const mongoose = require("mongoose");
+const routes = require("./routes");
 const app = express();
-
-
 const PORT = process.env.PORT || 5000;
 
-// Checking application environment
-// console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
-console.log(`NODE_ENV: ${app.get('env')}`);
-
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
-
-//Adding middleware
-
-app.use(cors());
-app.use(bodyparser.json({ limit: '50mb', extended: true }));
-// bodyParser = {
-//     json: { limit: '50mb', extended: true },
-//     urlencoded: { limit: '50mb', extended: true }
-// }
-// app.use(bodyParser);
-app.use(nocache());
-app.use(express.static(__dirname + '/dist/quotationTool/'));
-require('./startup/routes')(app);
-require('./startup/db')();
-
-if (process.env.NODE_ENV == 'production') {
-    require('./startup/prod')(app);
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 }
+// Add routes, both API and view
+app.use(routes);
 
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
 
 // Start the API server
 app.listen(PORT, function () {
